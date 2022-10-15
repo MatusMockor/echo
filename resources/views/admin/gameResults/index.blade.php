961836b10b71514e1f41eef4cdd0186d7c193f1d
Matúš Močkor <codex611@gmail.com>
2022-10-15 21:06:46 +0200

    ADDED: show all game results

---
+++ resources/views/admin/gameResults/index.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.master')
+
+@section('title', 'All Teams')
+
+@section('content')
+
+    <div class="col-12 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <div class="list-group">
+                    @foreach($games as $game)
+                        <a href="{{route('gameResult.edit', ['gameResult' => $game->id])}}"
+                           class="list-group-item list-group-item-action flex-column align-items-start">
+                            <div class="d-flex w-100 justify-content-between">
+                                <h4 class="mb-1">{{$game->our_name}} Vs {{$game->enemy_name}}</h4>
+                                <p>{{$game->our_score}} : {{$game->enemy_score}}</p>
+                                <time datetime="{{$game->created_at->toW3cString()}}">
+                                    {{ $game->created_at->diffForHumans() }}
+                                </time>
+                            </div>
+                        </a>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
