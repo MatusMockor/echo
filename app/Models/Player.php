44b32791be33e0ecd8c2d98d75651de556efcaa4
Matúš Močkor <codex611@gmail.com>
2022-10-15 22:43:45 +0200

    ADDED: fillable properties for player model

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -2,34 +2,45 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
 /**
  * App\Models\Player
  *
- * @property int $id
- * @property int $team_id
- * @property string $name
- * @property string $slug
- * @property int $age
- * @property string $description
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @method static \Illuminate\Database\Eloquent\Builder|Player newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Player newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Player query()
- * @method static \Illuminate\Database\Eloquent\Builder|Player whereAge($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Player whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Player whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Player whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Player whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Player whereSlug($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Player whereTeamId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Player whereUpdatedAt($value)
+ * @property int         $id
+ * @property int         $team_id
+ * @property string      $name
+ * @property string      $slug
+ * @property int         $age
+ * @property string      $description
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Player newModelQuery()
+ * @method static Builder|Player newQuery()
+ * @method static Builder|Player query()
+ * @method static Builder|Player whereAge($value)
+ * @method static Builder|Player whereCreatedAt($value)
+ * @method static Builder|Player whereDescription($value)
+ * @method static Builder|Player whereId($value)
+ * @method static Builder|Player whereName($value)
+ * @method static Builder|Player whereSlug($value)
+ * @method static Builder|Player whereTeamId($value)
+ * @method static Builder|Player whereUpdatedAt($value)
  * @mixin \Eloquent
  */
 class Player extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'team_id',
+        'name',
+        'slug',
+        'age',
+        'description',
+    ];
+
 }
