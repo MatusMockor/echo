6e12f8b5c1221f54c825530f4ad42c291753e8f8
Matúš Močkor <codex611@gmail.com>
2022-10-15 21:05:58 +0200

    CHANGED: use route model binding for edit game result

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::middleware(['auth'])->group(function () {
             Route::get('/', [GameResultController::class, 'index'])->name('gameResult.index');
             Route::get('/create', [GameResultController::class, 'create'])->name('gameResult.create');
             Route::post('/', [GameResultController::class, 'store'])->name('gameResult.store');
-            Route::get('/{game}/edit', [GameResultController::class, 'edit'])->name('gameResult.edit');
+            Route::get('/{gameResult}/edit', [GameResultController::class, 'edit'])->name('gameResult.edit');
         });
     });
 });
