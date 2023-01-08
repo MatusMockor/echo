e01eaf71f94ec95d81796df1904bcd70a694afdb
Matúš Močkor <codex611@gmail.com>
2023-01-08 17:14:36 +0100

    REMOVED: not exists components from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,17 +3,13 @@ import './libs/trix';
 import {createApp} from "vue";
 import mitt from "mitt";
 import FlashMessageAlert from "./components/admin/FlashMessageAlert.vue";
-import CreateTeamForm from "./components/admin/CreateTeamForm.vue";
 import TeamForm from "./components/admin/TeamForm.vue";
-import EditTeamForm from "./components/admin/EditTeamForm.vue";
 import GameResultForm from "./components/admin/GameResultForm.vue";
 
 window.eventBus = mitt();
 
 const app = createApp({});
 app.component('FlashMessageAlert', FlashMessageAlert);
-app.component('CreateTeamForm', CreateTeamForm);
-app.component('EditTeamForm', EditTeamForm);
 app.component('TeamForm', TeamForm);
 app.component('GameResultForm', GameResultForm);
 
