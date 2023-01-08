8357a7792520b4b10d6301658075a84900cf219d
Matúš Močkor <codex611@gmail.com>
2023-01-08 17:14:12 +0100

    CHANGED: use team form component for create and edit actions

--- resources/views/admin/teams/create.blade.php
+++ resources/views/admin/teams/create.blade.php
@@ -7,9 +7,9 @@
         <div class="card">
             <div class="card-body">
                 <h4 class="card-title">Now you can create amazing Team section</h4>
-                <create-team-form
-                        route-send-form="{{route('players.store')}}"
-                        route-back="{{ route('players.index')  }}"
+                <team-form
+                        route-send-form="{{route('teams.store')}}"
+                        route-back="{{ route('teams.index')  }}"
                 />
             </div>
         </div>
